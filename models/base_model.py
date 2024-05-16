99b3584dfc1aae5614dfa570661d521e5708055c
Abdul-Qudus <akindelequdus2@gmail.com>
2024-05-16 02:24:45 -0700

    basemodel

---
+++ models/base_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__ (self):
+        return 
