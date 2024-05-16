7ff84ab7e9363adc5f5d2d51a3545242d99fe5d1
Abdul-Qudus <akindelequdus2@gmail.com>
2024-05-16 02:54:46 -0700

    return instance

--- models/base_model.py
+++ models/base_model.py
@@ -6,9 +6,12 @@ from datetime import datetime
 
 class BaseModel:
     def __init__(self):
+        """Initializes a new instance of BaseModel"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
     def __str__ (self):
-        return 
+        """Returns a string representation of the instance"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
