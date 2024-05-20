56e2fe1972612bdb4aa5d10fa921e70a2df2b6de
Abdul-Qudus <akindelequdus2@gmail.com>
2024-05-19 21:36:38 -0700

    cmd

---
+++ console.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Console Module
+"""
+import re
+import shlex
+import ast
+import cmd
+from models import storage
+from models.base_model import BaseModel
+
+class HBNBCommand(cmd.Cmd):
+    """
+    HBNBCommand class console
+    """
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """
+        Quit command for  exit of  the program.
+        """
+        return True
+
+    def do_EOF(self, arg):
+        """
+        EOF (Ctrl+D) a  signal for  exiting the program.
+        """
+        return True
+
+    def emptyline(self):
+        """
+        Does nothing when an empty line is entered.
+        """
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
